3e7da2f6e13d69f3b924f3fcb1cfdcb11d401515
Subhasis Jethy <subhasis.jethy01@sap.com>
2020-06-17 16:33:49 +0530

    model_saved

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import os
 
 EPOCHS = 15
-MODEL_SAVE_PATH = os.path.join('Saved_Models','facemaskdetector.pth')
+MODEL_SAVE_PATH = os.path.join('saved_models','facemaskdetector.pth')
 BATCH_SIZE = 32
 TRAINING_DATA = os.path.join('data','train')
 TESTING_DATA = os.path.join('data','test')
