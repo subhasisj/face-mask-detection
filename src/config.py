2585f1db95cea3e80b02dbced0a63b6260c42d82
Subhasis Jethy <subhasis.jethy01@sap.com>
2020-06-22 23:25:38 +0530

    Inferencing with GPU

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import os
 
 EPOCHS = 15
-MODEL_SAVE_PATH = os.path.join('..','saved_models','facemaskdetection_new_mask_types.pth')
+MODEL_SAVE_PATH = os.path.join('..','saved_models','facemaskdetection_new_mask_types2020-06-22.pth')
 BATCH_SIZE = 32
 TRAINING_DATA = os.path.join('data','train')
 TESTING_DATA = os.path.join('data','test')
